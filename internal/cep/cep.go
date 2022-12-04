50904394d58d55e5a0a0ffb87f08ea84b75e952a
Wevyrton <wevyrton@gmail.com>
2022-12-04 18:48:14 -0300

    criado rota BuscarCep

--- internal/cep/cep.go
+++ internal/cep/cep.go
@@ -11,12 +11,12 @@ import (
 )
 
 type Logradouro struct {
-	Rua     string         `json:"Rua"`
-	Numero  int            `json:"Numero"`
-	Bairro  string         `json:"Bairro"`
-	Cidade  string         `json:"Cidade"`
-	Uf      string         `json:"Uf"`
-	Pessoas pessoas.Pessoa `json:"Morador"`
+	Rua    string         `json:"rua"`
+	Numero int            `json:"numero"`
+	Bairro string         `json:"bairro"`
+	Cidade string         `json:"cidade"`
+	Uf     string         `json:"uf"`
+	Pessoa pessoas.Pessoa `json:"pessoa"`
 }
 
 var morador0 pessoas.Pessoa = pessoas.Pessoas[0]
@@ -25,62 +25,63 @@ var morador2 pessoas.Pessoa = pessoas.Pessoas[2]
 
 var Enderecos = []Logradouro{
 	{
-		Rua:     "Henriqueta Lisboa",
-		Numero:  100,
-		Bairro:  "Jardim Amanda",
-		Cidade:  "Hortolândia",
-		Uf:      "São Paulo",
-		Pessoas: morador0,
+		Rua:    "Henriqueta Lisboa",
+		Numero: 100,
+		Bairro: "Jardim Amanda",
+		Cidade: "Hortolândia",
+		Uf:     "São Paulo",
+		Pessoa: morador0,
 	},
 
 	{
-		Rua:     "Helizia Machado Benassi",
-		Numero:  333,
-		Bairro:  "Nova Cidade Jardim",
-		Cidade:  "Jundiai",
-		Uf:      "São Paulo",
-		Pessoas: morador1,
+		Rua:    "Helizia Machado Benassi",
+		Numero: 333,
+		Bairro: "Nova Cidade Jardim",
+		Cidade: "Jundiai",
+		Uf:     "São Paulo",
+		Pessoa: morador1,
 	},
 
 	{
-		Rua:     "Borba Gato",
-		Numero:  28,
-		Bairro:  "Comendador Soares",
-		Cidade:  "Nova Iguaçu",
-		Uf:      "Rio de Janeiro",
-		Pessoas: morador2,
+		Rua:    "Borba Gato",
+		Numero: 28,
+		Bairro: "Comendador Soares",
+		Cidade: "Nova Iguaçu",
+		Uf:     "Rio de Janeiro",
+		Pessoa: morador2,
 	},
 	{
-		Rua:     "Henriqueta Lisboa",
-		Numero:  100,
-		Bairro:  "Jardim Amanda",
-		Cidade:  "Hortolândia",
-		Uf:      "São Paulo",
-		Pessoas: pessoas.Pessoa{Id: 04, Nome: "Gabriel", Senha: "2609"},
-	},
-
-	{
-		Rua:     "Helizia Machado Benassi",
-		Numero:  333,
-		Bairro:  "Nova Cidade Jardim",
-		Cidade:  "Jundiai",
-		Uf:      "São Paulo",
-		Pessoas: pessoas.Pessoa{},
+		Rua:    "Henriqueta Lisboa",
+		Numero: 100,
+		Bairro: "Jardim Amanda",
+		Cidade: "Hortolândia",
+		Uf:     "São Paulo",
+		Pessoa: pessoas.Pessoa{Id: 04, Nome: "Gabriel", Senha: "2609"},
 	},
 
 	{
-		Rua:     "Afonso Pena",
-		Numero:  2558,
-		Bairro:  "Abolição",
-		Cidade:  "Campinas",
-		Uf:      "São Paulo",
-		Pessoas: pessoas.Pessoa{Id: 06, Nome: "Isabelle", Senha: "bf!!@#$¨&*(DGHae22212"},
+		Rua:    "Helizia Machado Benassi",
+		Numero: 333,
+		Bairro: "Nova Cidade Jardim",
+		Cidade: "Jundiai",
+		Uf:     "São Paulo",
+		Pessoa: pessoas.Pessoa{},
+	},
+
+	{
+		Rua:    "Afonso Pena",
+		Numero: 2558,
+		Bairro: "Abolição",
+		Cidade: "Campinas",
+		Uf:     "São Paulo",
+		Pessoa: pessoas.Pessoa{Id: 06, Nome: "Isabelle", Senha: "bf!!@#$¨&*(DGHae22212"},
 	},
 }
 
 func ListarCep(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(Enderecos)
 }
+
 func BuscarCep(w http.ResponseWriter, r *http.Request) {
 	id := chi.URLParam(r, "id")
 	idint, err := strconv.Atoi(id)
@@ -89,7 +90,7 @@ func BuscarCep(w http.ResponseWriter, r *http.Request) {
 		json.NewEncoder(w).Encode("NAO EXISTE ESSE USUARIO")
 	}
 	for _, cep := range Enderecos {
-		if cep.Pessoas.Id == idint {
+		if cep.Pessoa.Id == idint {
 			json.NewEncoder(w).Encode(cep)
 
 		}
@@ -97,12 +98,21 @@ func BuscarCep(w http.ResponseWriter, r *http.Request) {
 
 	fmt.Println(" funcionando...BuscarCep GET...")
 }
+
 func CriarCep(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("logradouro funcionando...CriarLogradouro")
+	var morador Logradouro
+	err := json.NewDecoder(r.Body).Decode(&morador)
+	if err != nil {
+		panic(err)
+	}
+	Enderecos = append(Enderecos, morador)
+
 }
+
 func AtualizarCep(w http.ResponseWriter, r *http.Request) {
 	fmt.Println(" funcionando...CriarCep PUT...")
 }
+
 func DeletarCep(w http.ResponseWriter, r *http.Request) {
 	fmt.Println(" funcionando...DeletarCep DELETE...")
 }
